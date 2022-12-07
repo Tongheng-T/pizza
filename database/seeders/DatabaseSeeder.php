98a8e9ce9c260ef8b62e60cd369f9b4e0e55e66d
Tongheng-T <tonghengit@gmail.com>
2022-12-07 22:26:27 +0700

    middleware added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Pizza;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
