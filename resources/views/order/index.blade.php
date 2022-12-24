d0b6f5afb23e09738e42ea2239fd59f0857b27ee
Tongheng-T <tonghengit@gmail.com>
2022-12-24 08:38:03 +0700

    userorder dashboard,filter,frontpage

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-12">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
@@ -11,8 +11,10 @@
                 </nav>
 
                 <div class="card">
-                    <div class="card-header">Order</div>
-
+                    <div class="card-header">Order
+                        <a style="float:right;" href="{{route('pizza.index')}}"><button class="bnt btn-secondary btn-sm" style="margin-left: 5px;">View Pizza</button></a>
+                    <a style="float:right;" href="{{route('pizza.create')}}"><button class="bnt btn-secondary btn-sm">Add new Pizza</button></a>
+                    </div>
                     <div class="card-body">
                         <table class="table table-bordered">
                             <thead>
